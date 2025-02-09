25daf734124b3658670cb7c96892df0a761ae8ea
Thibus-png <thibaut.penet2@edu.devinci.fr>
2025-02-09 22:41:19 +0100

    Add files via upload

---
+++ landing-page.js
@@ -0,0 +1,20 @@
+document.getElementById("exchangeBtn").addEventListener("click", function() {
+    document.getElementById("message").innerText = "L'échange commence ! Prépare tes cartes !";
+});
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
